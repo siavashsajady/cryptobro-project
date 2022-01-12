715cbe9c343d1ea40946d757438d982b9d58a49a
siavashsajady <hubgitpro@gmail.com>
2022-01-12 17:26:33 +0100

    create layout of main content & create main componets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import {
+  Navbar,
+  Exchanges,
+  Cryptocurrencies,
+  News,
+  HomePage,
+  CryptoDetails,
+} from './components';
 import './App.css';
 
 const App = () => {
@@ -11,10 +18,35 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-        <h1>main page</h1>
-      </div>
-      <div className='footer'>
-        <h1>Footer</h1>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<HomePage />}></Route>
+              <Route path='/exchanges' element={<Exchanges />}></Route>
+              <Route
+                path='/cryptocurrencies'
+                element={<Cryptocurrencies />}
+              ></Route>
+              <Route path='/crypto/:coinId' element={<CryptoDetails />}></Route>
+              <Route exact path='/news' element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            Coptobro © 2022
+            <Link to='/'>Cryptobro Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CryptoDetails = () => {
+  return (
+    <div>
+      <h3>hello</h3>
+    </div>
+  );
+};
+
+export default CryptoDetails;
