a9bf21adf36ba798b68fff36ecfacb4e6989c7ef
siavashsajady <hubgitpro@gmail.com>
2022-01-18 17:26:03 +0100

    fetch cryptocurrency data from rapid api & install redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'dc81937d52mshd8d416da0ca6886p12e90bjsn1f365b5b53d0',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
